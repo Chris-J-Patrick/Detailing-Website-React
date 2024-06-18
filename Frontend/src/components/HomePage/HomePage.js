16e1febe7d58341ecac1fee259bf8ca0d3691291
Christopher <chrispatrick96@outlook.com>
2024-06-18 19:13:23 -0400

    removed homepage logo

--- Frontend/src/components/HomePage/HomePage.js
+++ Frontend/src/components/HomePage/HomePage.js
@@ -38,16 +38,6 @@ function HomePage() {
       )}
 
       <div className="Welcome d-flex flex-column justify-content-evenly align-items-center ">
-        <div className="text-center">
-          <LogoDiamond
-            size={2.5}
-            sizeUnit="rem"
-            color="white"
-            textColor="white"
-            textFontSize={2.5}
-            textFontSizeUnit="rem"
-          />
-        </div>
         <p className="fs-5 text-center">
           South Jersey's destination for{" "}
           <span className="Highlight">premier car detailing</span>. Experience{" "}
