af6f593539d2a73f1fcf30f4a7cb287bf508fdb7
Christopher <chrispatrick96@outlook.com>
2024-07-02 13:30:08 -0400

    modified:   Frontend/src/components/LogoutButton.js

--- Frontend/src/components/LogoutButton.js
+++ Frontend/src/components/LogoutButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import "react-bootstrap";
+
+import { Button } from "react-bootstrap";
 
 const LogoutButton = () => {
   const { logout, isAuthenticated } = useAuth0();
