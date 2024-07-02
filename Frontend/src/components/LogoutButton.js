1983921d32ee64a307abacf6d82f64d55cb9d6d2
Christopher <chrispatrick96@outlook.com>
2024-07-02 13:23:46 -0400

    modified:   Frontend/src/components/LogoutButton.js

--- Frontend/src/components/LogoutButton.js
+++ Frontend/src/components/LogoutButton.js
@@ -1,22 +1,19 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Button } from "react-bootstrap";
 
 const LogoutButton = () => {
-  const { logout, isAuthenticated } = useAuth0();
+  const { logout } = useAuth0();
 
   return (
-    isAuthenticated && (
-      <Button
-        onClick={() =>
-          logout({
-            logoutParams: { returnTo: "http://localhost:3000/rewards" },
-          })
-        }
-      >
-        Log Out
-      </Button>
-    )
+    <button
+      onClick={() =>
+        logout({
+          logoutParams: { returnTo: window.location.origin + "/rewards" },
+        })
+      }
+    >
+      Log Out
+    </button>
   );
 };
 
