857c02eef26da33b9e21f51c19f66426982951b1
Christopher <chrispatrick96@outlook.com>
2024-07-02 13:16:20 -0400

    modified:   Frontend/src/index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -11,7 +11,7 @@ root.render(
     domain="dev-8axoaj12hkg375lw.us.auth0.com"
     clientId="YjUXTDjeKwvj5C471rsak54365k0V0Wi"
     authorizationParams={{
-      redirect_uri: window.location.origin + "/rewards",
+      redirect_uri: window.location.origin,
     }}
   >
     <App />
